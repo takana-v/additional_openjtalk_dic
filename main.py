0f430166009d9f6c6427a511aed7d7ad6af6d65e
takana-v <44311840+takana-v@users.noreply.github.com>
2021-11-04 00:14:16 +0900

    fix miss

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ with open("neologd_all.csv", newline="", encoding="utf-8") as f:
     with open("additional_openjtalk_dic.csv", mode="w",newline="", encoding="utf-8") as f2:
         csv_writer = csv.writer(f2)
         for l in tqdm(csv_reader):
-            add_l = [to_zenkaku.translate(l[0])]
+            add_l = [(l[0].translate(to_zenkaku))]
             add_l += l[1:13]
             add_l.append(f"{converter.sy2a(l[0],l[12]).find('[')+1}/{len(prog.findall(l[12]))}")
             add_l.append("*")
