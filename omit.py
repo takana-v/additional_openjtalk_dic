384693ac51b800db336aa62986cad7ffe515a7a8
takana-v <44311840+takana-v@users.noreply.github.com>
2021-11-05 00:18:37 +0900

    Update omit.py

--- omit.py
+++ omit.py
@@ -12,6 +12,7 @@ with open("neologd_all.csv", encoding="utf-8", newline="") as f:
         preprocess(f, f2)
 
 converter = Converter()
+to_zenkaku = str.maketrans("".join(chr(0x21 + i) for i in range(94)), "".join(chr(0xff01 + i) for i in range(94)))
 prog = re.compile('(?:[アイウエオカ-モヤユヨ-ロワ-ヶ][ァィゥェォャュョヮ]|[アイウエオカ-モヤユヨ-ロワ-ヶー])')
 with open("neologd_omitted.csv", newline="", encoding="utf-8") as f:
     for i, _ in enumerate(f):
@@ -21,7 +22,8 @@ with open("neologd_omitted.csv", newline="", encoding="utf-8") as f:
     with open("additional_openjtalk_omitted_dic.csv", mode="w",newline="", encoding="utf-8") as f2:
         csv_writer = csv.writer(f2)
         for l in tqdm(csv_reader, total=i+1):
-            add_l = l[0:13]
+            add_l = [(l[0].translate(to_zenkaku))]
+            add_l += l[1:13]
             add_l.append(f"{converter.sy2a(l[0],l[12]).find('[')+1}/{len(prog.findall(l[12]))}")
             add_l.append("*")
             csv_writer.writerow(add_l)
